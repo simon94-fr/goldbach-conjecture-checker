7d9e2c5e03dbf31d139572d367fa14927a790b5b
simon94-fr <therealsimonkerneis@gmail.com>
2025-09-11 07:45:15 +0100

    start

---
+++ script.js
@@ -0,0 +1,10 @@
+let a;
+function isPrime(x){
+    if (x => 0 && Number.isInteger(x)) {
+        if(x <= 1) return false
+        if(x <= 3) return true
+        if(x % 2 === || x % 3 === 0) return false
+    } else {
+        return false;
+    } 
+}
